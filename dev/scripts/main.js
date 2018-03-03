4965a6de48afe6f1dcec8b57dae3039849b14a40
loreerol <lorehill.hello@gmail.com>
2018-03-03 15:00:14 -0500

    initial commit

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,4 +1,10 @@
-const ohHey = "Hello World";
+//poem generator
 
-console.log(ohHey);
-console.log("works");
+//User can choose from a number of words, each word has a mood value
+//
+//Once the words are chosen, the mood value is calculated and a madlibs style template is chosen
+//
+//The chosen words are inserted into the madlib and shown to the user
+//
+//*bonus, the user can download the poem as .txt
+
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,10 @@
+//poem generator
+
+//User can choose from a number of words, each word has a mood value
+//
+//Once the words are chosen, the mood value is calculated and a madlibs style template is chosen
+//
+//The chosen words are inserted into the madlib and shown to the user
+//
+//*bonus, the user can download the poem as .txt
 "use strict";
-
-var ohHey = "Hello World";
-
-console.log(ohHey);
-console.log("works");
